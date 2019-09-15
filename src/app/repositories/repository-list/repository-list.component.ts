502b78409be09a0078fc0ec97ca8fe5ee0a5fafe
azelsky <andry.zelinskiy@gmail.com>
2019-09-15 03:05:58 +0300

    add filter by language

--- src/app/repositories/repository-list/repository-list.component.ts
+++ src/app/repositories/repository-list/repository-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {IRepository} from '../../models/repository';
 
 @Component({
@@ -6,13 +6,32 @@ import {IRepository} from '../../models/repository';
   templateUrl: './repository-list.component.html',
   styleUrls: ['./repository-list.component.scss']
 })
-export class RepositoryListComponent implements OnInit {
+export class RepositoryListComponent implements OnChanges {
   @Input() repositories: IRepository[] = [];
   @Input() totalCount: number;
+  selectedLanguages: string[] = [];
+  languageList: string[] = [];
+  notDefined = 'Not defined';
 
   constructor() {}
 
-  ngOnInit() {
+  ngOnChanges() {
+    this.getUniqueLanguages();
   }
 
+  getUniqueLanguages(): void {
+    const languages = this.repositories.map((repository: IRepository) => repository.language || this.notDefined);
+    this.languageList = [...new Set(languages)];
+  }
+
+  getFilteredRepositories(): IRepository[] {
+    return this.repositories.filter((repository: IRepository) => {
+      const language = repository.language || this.notDefined;
+      if (!this.selectedLanguages.length) {
+        return true;
+      }
+
+      return this.selectedLanguages.includes(language);
+    });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
@@ -11,11 +13,15 @@ import {MatIconModule} from '@angular/material/icon'
     CommonModule,
     MatInputModule,
     MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   exports: [
     MatInputModule,
     MatCardModule,
     MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ]
 })
 export class SharedModule { }
